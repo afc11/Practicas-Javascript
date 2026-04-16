184c05aa0b66b425b35f190c162e8b6133695106
Agustin <albertofernandez11905@gmail.com>
2026-04-15 21:36:36 -0300

    Ejercicio 2

---
+++ ejercicios/variables_y_condicionales/script.js
@@ -0,0 +1,21 @@
+let nombre = "Alberto"
+
+if (nombre !== "") {
+    console.log("Hola " + nombre)
+} else{
+    console.log("Hola desconosido")
+}
+
+
+
+
+const numero = 7;
+
+if ( numero % 2 === 0) {
+    console.log(numero + " es par")
+} else{
+    console.log(numero + " es impar")
+}
+
+
+
