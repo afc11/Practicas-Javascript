8e85d450d94889f2cf24d4e1d867ffab1ca27141
Agustin <albertofernandez11905@gmail.com>
2026-04-20 21:07:45 -0300

    Ejercicio 1

---
+++ ejercicios/bucles_y_arrays/script.js
@@ -0,0 +1,14 @@
+
+const numero = [1,2,3,4,5,6,7,8,9,10]
+let suma = 0
+
+for (let i = 0; i < numero.length; i++) {
+suma = suma + numero[i]
+}
+
+console.log("La longitud del array es: " + numero.length)
+console.log("Suma total: " + suma)
+
+
+
+
