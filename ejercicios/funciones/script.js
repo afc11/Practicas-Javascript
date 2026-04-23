43a3a23475be0d510811f8d58b6a30b6e47993fa
Agustin <albertofernandez11905@gmail.com>
2026-04-22 21:48:33 -0300

    Ejercicio 6

--- ejercicios/funciones/script.js
+++ ejercicios/funciones/script.js
@@ -50,3 +50,18 @@ const celsiusAFahrenheit = (celsius) => {
 
 
 
+const calificarNota = (nota) => {
+    if (nota >= 90) {
+        return "A"
+    } else if (nota >= 80) {
+        return "B"
+    } else if (nota >= 70) {
+        return "C"
+    } else {
+        return "F"
+    }
+}
+
+
+
+
