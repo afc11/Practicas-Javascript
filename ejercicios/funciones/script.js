0d1007fc5901fa64cebcf0a9802bdd3c10dac8c8
Agustin <albertofernandez11905@gmail.com>
2026-04-22 21:25:27 -0300

    Ejercicio 2

--- ejercicios/funciones/script.js
+++ ejercicios/funciones/script.js
@@ -7,3 +7,18 @@ function saludar(nombre) {
 
 
 
+function calculadora(num1, num2, operador) {
+    switch (operador) {
+        case "+":
+            return num1 + num2
+        case "-":
+            return num1 - num2
+        case "/":
+            return num1 / num2
+        case "*":
+            return num1 * num2
+        default:
+            return "Operador no válido"
+    }
+}
+
