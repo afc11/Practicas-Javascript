a0ad43d675ff44f3063f26118863356811d57b7f
Agustin <albertofernandez11905@gmail.com>
2026-04-27 22:00:06 -0300

    Ejercicio 1

---
+++ ejercicios/interaccion_con_el_DOM/script.js
@@ -0,0 +1,11 @@
+
+const titulo = document.getElementById("titulo")
+const btn = document.getElementById("btn")
+
+btn.addEventListener("click", () => {
+    titulo.textContent = "Texto cambiado"
+})
+
+
+
+
