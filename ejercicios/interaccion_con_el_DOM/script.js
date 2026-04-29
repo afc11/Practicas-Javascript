76c79ab2f2b83ba0ae771325d9de3610f56bb8b8
Agustin <albertofernandez11905@gmail.com>
2026-04-29 20:36:55 -0300

    Ejercicio 3

--- ejercicios/interaccion_con_el_DOM/script.js
+++ ejercicios/interaccion_con_el_DOM/script.js
@@ -15,3 +15,17 @@ const BTN = document.getElementById("btnToggle")
 BTN.addEventListener("click", () => {
     texto.classList.toggle("activo")
 })
+
+
+
+
+const input = document.getElementById("input")
+const btnAgregar = document.getElementById("btnAgregar")
+const lista = document.getElementById("lista")
+
+btnAgregar.addEventListener("click", () => {
+    const li = document.createElement("li")
+    li.textContent = input.value
+    lista.appendChild(li)
+    input.value = ""
+ })
