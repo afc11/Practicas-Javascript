b140ee90355ddb0924e5722ff89ff06bc2958d38
Agustin <albertofernandez11905@gmail.com>
2026-05-06 20:40:11 -0300

    a

---
+++ ejercicios/Asincronía y Fetch API/script.js
@@ -0,0 +1,24 @@
+const lista = document.querySelector("#lista")
+
+const cargarPokemons = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
+        .then((rec) => {
+if (!rec.ok)throw new Error("Error al llamar a la API");
+            return rec.json();
+        })   
+        .then((data) => {
+           const pokemons = data.results.slice(0, 3);
+            console.log(pokemons);
+
+            pokemons.map((pokemon) => {
+                const li = document.createElement("li");
+                li.textContent = pokemon.name;
+                lista.appendChild(li);
+            });
+
+    });
+        .catch((error) => console.error(error));
+
+     }
+
+     cargarPokemons();
